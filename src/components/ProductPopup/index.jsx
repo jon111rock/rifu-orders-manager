c9d37ea05b69e5fbde2b3e3a3dc6cdf5e7fb4c29
yu teng hsueh <jon111rock@gmail.com>
2022-06-02 22:02:15 +0800

    Post newItem and fetech itemList

--- src/components/ProductPopup/index.jsx
+++ src/components/ProductPopup/index.jsx
@@ -1,24 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./style.scss";
 
-const ProductPopup = () => {
+const ProductPopup = (props) => {
+  const [name, setName] = useState();
+  const [price, setPrice] = useState();
+
+  const clearUp = () => {
+    setName();
+    setPrice();
+  };
+
+  const postProduct = async () => {
+    try {
+      await axios.post("http://localhost:3500/api/item", {
+        name: name,
+        price: price,
+      });
+      props.fetechProductData();
+    } catch (error) {}
+  };
+
+  const handleClickSave = () => {
+    if (!name || !price) return;
+    postProduct();
+    clearUp();
+    props.setIsOpenProductPopup(false);
+  };
+
   return (
     <div className="p-popup">
       <div className="p-popup-inner">
         <div className="d-input-box">
           <div className="d-input-item">
             <span>名稱</span>
-            <input className="p-input" type="text" />
+            <input
+              className="p-input"
+              type="text"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
           </div>
           <div className="d-input-item">
             <span>價格</span>
-            <input className="p-input" type="text" />
+            <input
+              className="p-input"
+              type="text"
+              onChange={(e) => {
+                setPrice(e.target.value);
+              }}
+            />
           </div>
         </div>
 
         <div className="d-button-box">
-          <button className="save">儲存</button>
-          <button className="cancel">取消</button>
+          <button
+            className="save"
+            onClick={() => {
+              handleClickSave();
+            }}
+          >
+            儲存
+          </button>
+          <button
+            className="cancel"
+            onClick={() => {
+              clearUp();
+              props.setIsOpenProductPopup(false);
+            }}
+          >
+            取消
+          </button>
         </div>
       </div>
     </div>
--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./style.scss";
 import ProductPopup from "../../components/ProductPopup";
 
 const Product = () => {
+  const [isOpenProductPopup, setIsOpenProductPopup] = useState(false);
+  const [productList, setProductList] = useState([]);
+
+  //fetechProductData
+  const fetechProductData = async () => {
+    const res = await axios.get("http://localhost:3500/api/item");
+
+    setProductList(res.data.result);
+    // console.log(res.data.result);
+  };
+
+  useEffect(() => {
+    fetechProductData();
+  }, []);
+
   return (
     <>
       <div className="dashboard">
@@ -11,29 +27,37 @@ const Product = () => {
             <i className="bx bx-menu bx-lg menu-logo"></i>
           </label>
           <h2>產品</h2>
-          <div className="add-product">
+          <div
+            className="add-product"
+            onClick={() => {
+              setIsOpenProductPopup(true);
+            }}
+          >
             <i className="bx bx-plus bx-sm"></i>
             <span>新增產品</span>
           </div>
         </div>
         <div className="product-container">
           <ul className="product-list">
-            <li className="product-item">
-              <h3>草莓三明治</h3>
-              <h3>35$</h3>
-            </li>
-            <li className="product-item">
-              <h3>草莓三明治</h3>
-              <h3>35$</h3>
-            </li>
-            <li className="product-item">
-              <h3>草莓三明治</h3>
-              <h3>35$</h3>
-            </li>
+            {productList.map((product) => {
+              return (
+                <li className="product-item" key={product._id}>
+                  <h3>{product.name}</h3>
+                  <h3>$ {product.price}</h3>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
-      {/* <ProductPopup /> */}
+      {isOpenProductPopup ? (
+        <ProductPopup
+          setIsOpenProductPopup={setIsOpenProductPopup}
+          fetechProductData={fetechProductData}
+        />
+      ) : (
+        ""
+      )}
     </>
   );
 };
