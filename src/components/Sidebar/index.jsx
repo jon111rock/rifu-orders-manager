76fdaf8da64699b9ed879bf0db2709fa749ea99e
yu teng hsueh <jon111rock@gmail.com>
2022-06-02 16:32:20 +0800

    Refactor sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,40 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style.scss";
 
 const Sidebar = () => {
+  const [clickTarget, setClickTarget] = useState("訂單");
+
   return (
     <nav className="sidebar">
       <label htmlFor="sidebar-switch" className="close-sidebar">
         <i className="bx bx-x bx-md"></i>
       </label>
       <div className="header">
-        <img src="https://picsum.photos/seed/1/100/100" alt="" />
-        <span className="content">SideBar</span>
+        {/* <img src="https://picsum.photos/seed/1/100/100" alt="" /> */}
+        <span className="content">日夫先生</span>
       </div>
       <div className="main">
         <ul>
-          <li>
-            <i className="bx bx-home-alt-2"></i>
-            <span>Dashboard</span>
+          <li
+            className={clickTarget === "訂單" ? "sidebar-active" : ""}
+            onClick={() => {
+              setClickTarget("訂單");
+            }}
+          >
+            <i className="bx bx-home-alt-2 "></i>
+            <span>訂單</span>
           </li>
-          <li>
+          <li
+            className={clickTarget === "產品" ? "sidebar-active" : ""}
+            onClick={() => {
+              setClickTarget("產品");
+            }}
+          >
             <i className="bx bxs-book-content"></i>
-            <span>Product</span>
-          </li>
-          <li>
-            <i className="bx bx-book-reader"></i>
-            <span>Order</span>
+            <span>產品</span>
           </li>
         </ul>
       </div>
       <div className="footer">
         <ul>
-          <li>
+          {/* <li>
             <i className="bx bx-cog"></i>
             <span>Setting</span>
-          </li>
+          </li> */}
           <li>
             <i className="bx bx-log-out"></i>
-            <span>Logout</span>
+            <span>登出</span>
           </li>
         </ul>
       </div>
