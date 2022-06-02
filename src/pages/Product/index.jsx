2b66f4c69fa1b1bc7b803d6125c0c981f5c2efe2
yu teng hsueh <jon111rock@gmail.com>
2022-06-02 20:17:07 +0800

    Init product page

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import "./style.scss";
 
 const Product = () => {
   return (
     <div className="dashboard">
-      <div>Product</div>
+      <div className="product-header">
+        <label className="sidebar-switch" htmlFor="sidebar-switch">
+          <i className="bx bx-menu bx-lg menu-logo"></i>
+        </label>
+        <h2>產品</h2>
+        <div className="add-product">
+          <i className="bx bx-plus bx-sm"></i>
+          <span>新增產品</span>
+        </div>
+      </div>
+      <div className="product-container">
+        <ul className="product-list">
+          <li className="product-item">
+            <h3>草莓三明治</h3>
+            <h3>35$</h3>
+          </li>
+          <li className="product-item">
+            <h3>草莓三明治</h3>
+            <h3>35$</h3>
+          </li>
+          <li className="product-item">
+            <h3>草莓三明治</h3>
+            <h3>35$</h3>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
