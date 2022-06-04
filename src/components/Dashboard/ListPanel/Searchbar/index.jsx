fba4db5dcf8427dadab113dea2c9d04204be64e9
yu teng hsueh <jon111rock@gmail.com>
2022-06-04 13:39:40 +0800

    Add searchBar ui control

--- src/components/Dashboard/ListPanel/Searchbar/index.jsx
+++ src/components/Dashboard/ListPanel/Searchbar/index.jsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+
+const drowDownList = ["依姓名搜尋", "依住址搜尋", "依電話搜尋"];
 
 const Searchbar = () => {
+  const dropdownRef = useRef();
+  const [isOpenDropdown, setIsOpenDropdown] = useState(false);
+  const [searchMode, setSearchMode] = useState(drowDownList[0]);
+
   return (
-    <div className="searchbar">
+    <div
+      className="searchbar"
+      onClick={(e) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+          setIsOpenDropdown(false);
+        }
+      }}
+    >
       <div className="search">
         <div className="s-dropdown-container">
           <div className="s-dropdown-btn">
             <i className="bx bx-search bx-sm"></i>
-            <input type="text" placeholder="依姓名搜尋" />
-            <i className="bx bxs-down-arrow"></i>
+            <input type="text" placeholder={searchMode} />
+            <i
+              ref={dropdownRef}
+              className="bx bxs-down-arrow"
+              onClick={() => {
+                setIsOpenDropdown(!isOpenDropdown);
+              }}
+            ></i>
           </div>
-          <ul className="s-dropdown-list active">
-            <li className="s-dropdown-item">item 1</li>
-            <li className="s-dropdown-item">item 2</li>
+          <ul className={`s-dropdown-list ${isOpenDropdown ? "active" : ""}`}>
+            {drowDownList.map((item, key) => (
+              <li
+                className="s-dropdown-item"
+                key={key}
+                onClick={() => {
+                  setSearchMode(drowDownList[key]);
+                }}
+              >
+                {item}
+              </li>
+            ))}
           </ul>
         </div>
       </div>
